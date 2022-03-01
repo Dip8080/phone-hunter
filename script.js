e6d0e307d661c44d209732e9a5f253f0b5b7009b
dip8080 <dipmollik7@gmail.com>
2022-03-01 22:31:19 +0600

    7th commit

--- script.js
+++ script.js
@@ -4,28 +4,57 @@ const clearContainer = id =>{
     clearArea.value= '';
 }
 
+// showeerror
+const showError = display =>{
+document.getElementById('show_error').style.display=display; 
+}
+const showError2 = display =>{
+document.getElementById('show_error2').style.display=display; 
+}
+
+
+// toogleSpinner
+
+const toogleSpinner = display =>{
+document.getElementById('loader').style.display=display;
+}
+
 document.getElementById('search_btn').addEventListener('click',()=>{
+    toogleSpinner('block');
     const inputField = document.getElementById('input_field');
     const inputValue = inputField.value ;
     if(inputValue==''){
-        alert('enter something')
+     const parent = document.getElementById('append_search');
+     const parent2 = document.getElementById('append_details');
+     parent2.textContent='';
+     parent.textContent='';
+      showError2('block');
+      showError('none');
+      toogleSpinner('none');  
     }
+    else{
+        showError2('none');
     clearContainer('input_field');
      fetch(`https://openapi.programming-hero.com/api/phones?search=${inputValue}`)
     .then(rcvData=>rcvData.json())
     .then(data => showSearchResult(data.data))
-/*     .catch(error=>{
-        alert('no data found',error)
-    }) */
-    
+} 
 })
 
 const showSearchResult = arrayOfObjects =>{
-    if(arrayOfObjects.length==0){
-        alert('no result found')
-    }
     const parent = document.getElementById('append_search');
     const parent2 = document.getElementById('append_details');
+
+    if(arrayOfObjects.length==0){
+        console.log('null array')
+       parent.textContent='';
+       parent2.textContent='';
+       showError('block');
+       showError2('none');
+       toogleSpinner('none')
+    }
+    else{
+        showError('none');
     parent2.textContent='';
     parent.textContent='';
     arrayOfObjects.slice(0,20)?.forEach(object => {
@@ -34,8 +63,9 @@ const showSearchResult = arrayOfObjects =>{
       <div onclick="fetchDetails('${object.slug}')" class="flex justify-center bg-cyan-300 rounded shadow p-5">
            <div>
                <img class="py-3" src="${object.image}" alt="">
-               <h1>brand : ${object.brand}</h1>
-           <p>Name : ${object.phone_name.slice(0,50)}</p>
+               <h1 class="shadow bg-cyan-300 p-2" >brand : ${object.brand}</h1>
+           <p class="shadow bg-cyan-300 p-2  mt-2">Name : ${object.phone_name.slice(0,50)}</p>
+           <button  class="mt-3 antialiased shadow-lg bg-cyan-400 p-2 rounded" onclick="fetchDetails('${object.slug}')">Details</button>
            </div>
        
       </div>
@@ -43,16 +73,20 @@ const showSearchResult = arrayOfObjects =>{
       
       parent.appendChild(newDiv);      
    });
+   toogleSpinner('none');
+}
 }
 
 const fetchDetails = id =>{
+    // toogleSpinner('block')
     fetch( `https://openapi.programming-hero.com/api/phone/${id}`)
     .then(rcvData=>rcvData.json())
     .then(data=>showDetails(data.data))
 }
 
 const showDetails = data =>{
-    console.log(data.releaseDate)
+    // console.log(data)
+   
    const parent = document.getElementById('append_details');
    parent.textContent='';
    const newDiv =document.createElement('div');
@@ -81,5 +115,7 @@ const showDetails = data =>{
      </div>
     </div>                  `
     parent.appendChild(newDiv)
+    // toogleSpinner('none')
+    
     
 }
