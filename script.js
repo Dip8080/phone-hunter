983de7d04cd7f08e51ad017e3104c2ec474d9206
dip8080 <dipmollik7@gmail.com>
2022-03-02 16:01:44 +0600

    8th commit

--- script.js
+++ script.js
@@ -60,12 +60,12 @@ const showSearchResult = arrayOfObjects =>{
     arrayOfObjects.slice(0,20)?.forEach(object => {
       const newDiv = document.createElement('div')
       newDiv.innerHTML= `
-      <div onclick="fetchDetails('${object.slug}')" class="flex justify-center bg-cyan-300 rounded shadow p-5">
+      <div class="flex justify-center bg-light-300 rounded shadow p-5">
            <div>
                <img class="py-3" src="${object.image}" alt="">
-               <h1 class="shadow bg-cyan-300 p-2" >brand : ${object.brand}</h1>
-           <p class="shadow bg-cyan-300 p-2  mt-2">Name : ${object.phone_name.slice(0,50)}</p>
-           <button  class="mt-3 antialiased shadow-lg bg-cyan-400 p-2 rounded" onclick="fetchDetails('${object.slug}')">Details</button>
+               <h1 class="shadow bg-light-300 p-2" >brand : ${object.brand}</h1>
+           <p class="shadow bg-light-300 p-2  mt-2">Name : ${object.phone_name.slice(0,50)}</p>
+           <a href=#append_details > <button href=#append_details class="mt-3 antialiased shadow-lg bg-cyan-400 p-2 rounded" onclick="fetchDetails('${object.slug}')">Details</button> </a>
            </div>
        
       </div>
@@ -82,22 +82,32 @@ const fetchDetails = id =>{
     fetch( `https://openapi.programming-hero.com/api/phone/${id}`)
     .then(rcvData=>rcvData.json())
     .then(data=>showDetails(data.data))
+    .catch(error=>{
+        alert('try another phone'+ " " + error )
+    })
 }
 
 const showDetails = data =>{
-    // console.log(data)
-   
+    // console.log(data.others)
+   const otherData =data.others;
+   const pair = Object.entries(otherData)
+   console.log(otherData); 
+   const Sensors = data.mainFeatures.sensors
+//    console.log(Sensors)
    const parent = document.getElementById('append_details');
    parent.textContent='';
    const newDiv =document.createElement('div');
    newDiv.innerHTML = `
-   <div class="grid g-3 grid-cols-1 md:grid-cols-2 lg:grid-cols-2  bg-cyan-300 rounded shadow p-3">
+   <div class="grid g-3 grid-cols-1 md:grid-cols-2 lg:grid-cols-2  bg-light-300 rounded shadow p-3">
+
    <div class="px-3 mt-6 w-86">
-       <img class="" src="${data.image}" alt="">
+       <img style="width:95%" class="" src="${data.image}" alt="">
        <h1 class=" antialiased text-xl mt-2">Brand :<span class="text-xl"> ${data.brand} </span></h1>
    </div>
+
      <div class=" py-2 px-4">
-   <h1 class=" antialiased text-xl mt-2">Name :<span class="text-base"> ${data.name} </span></h1>
+   <h1 class=" antialiased text-xl mt-2">Name :<span class="text-base"> ${data.name}
+    </span></h1>
    <p  class=" antialiased text-xl">Release date :<span class ="text-base">
     ${data.releaseDate.slice(0,50) ? data.releaseDate:'sorry, Not found'} <span></p>
    <p class = " pt-3 antialiased" > <span class="text-xl"> Mainfeature : </span>    <br>
@@ -106,15 +116,33 @@ const showDetails = data =>{
    memory : ${data.mainFeatures.memory} <br>
    storage : ${data.mainFeatures.storage} <br>
    </P>
-   <p class="mt-2 antialiased"> <span class="text-xl"> Sensors : </span><br>
-     ${data.mainFeatures.sensors[0]} <br>
-     ${data.mainFeatures.sensors[1]} <br>
-     ${data.mainFeatures.sensors[2]} <br>
-     ${data.mainFeatures.sensors[3]}
-   </p>
+  
+   
+   <h1 class="antialiased text-xl mt-2"> Sensors: </h1>
+    ${Sensors.map(x => ` ${x} `).join(',')}
+
+    <h1 class="antialiased text-xl mt-2"> Others: </h1>
+    <h1> ${pair.join('    ') ? pair:''} </h1>
+    
      </div>
-    </div>                  `
-    parent.appendChild(newDiv)
+    </div>  
+                
+    `
+
+parent.appendChild(newDiv)
+ 
+     
+ 
+ 
+  /*  const Sensors = data.mainFeatures.sensors;
+    Sensors.forEach(index =>{
+        
+        p.innerText =`${index} <br>`
+        newDiv.appendChild(p)
+    }) */
+   
+    // parent.appendChild(newDiv2)
+  
     // toogleSpinner('none')
     
     
